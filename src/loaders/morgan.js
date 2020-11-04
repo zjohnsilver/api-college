78fb564728e6debcb5ef0a6e2aaee4d6df491f78
john.silver <joao.prata@delfosim.com>
2020-11-04 15:46:19 -0300

    add morgan logger

---
+++ src/loaders/morgan.js
@@ -0,0 +1,8 @@
+import morgan from 'morgan'
+import moment from 'moment-timezone'
+
+morgan.token('date', (req, res, tz) => {
+  return moment().tz('America/Fortaleza').format('YYYY-MM-DD HH:mm')
+})
+
+export default morgan
--- src/server.js
+++ src/server.js
@@ -2,10 +2,11 @@ import express from 'express'
 import 'module-alias/register'
 import env from './config/env'
 import setupRoutes from './config/routes'
-
+import { Logger } from '@loaders'
 const PORT = env.port
 
 const app = express()
+  .use(Logger('[:date] :method :url  (:status)  :response-time ms'))
 setupRoutes(app)
 
 app.listen(PORT, () => {
